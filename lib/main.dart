6a5cf0352068cb66639c9367f89968dd136bc4f6
oamr <omarabdalkhaleq@mail.com>
2024-04-28 03:11:28 +0300

    start application

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:store/maro_store.dart';
+
+void main() {
+  runApp(const MaroStore());
+}
+
---
+++ lib/maro_store.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MaroStore extends StatelessWidget {
+  const MaroStore({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
